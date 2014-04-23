ec743c51a738a1685fdefa42d6f2b467fae94790
Llamasoft <gravisoft+git@gmail.com>
2014-04-23 15:34:57 -0500

    Better benchmark routine

--- bench.sh
+++ bench.sh
@@ -29,9 +29,6 @@ fi
 
 ##### PART ONE #####
 
-best_params=""
-best_cpm="-1"
-
 # Benchmark routine.  Many of these will fail.  That's ok, they'll fail quickly.
 for b in 24 25 26 27 28 29; do
     for s in 1 2 4 8 16 32; do
@@ -42,17 +39,15 @@ for b in 24 25 26 27 28 29; do
         echo "Currently Testing: ${cur_params}"
         timeout ${bench_length} ${miner_exe} ${base_params} ${cur_params} >> ${log_name} 2>&1
         
-        # Remove the log if the parameters are invalid, else load the speed
+        # Remove the log if the parameters are invalid, else log the speed
         if [[ -n "$(grep ERROR ${log_name})" ]]; then
             echo "           Result: Failure"
             rm ${log_name}
         else
-            echo "           Result: $(grep ^collisions ${log_name} | tail -n 1 | sed 's/;.*//')"
-            cur_cpm=$(grep ^collision ${log_name} | awk '{ print $2; }' | tail -n 1 | tr -d '.')
-            if (( cur_cpm > best_cpm )); then
-                best_cpm="${cur_cpm}"
-                best_params="${cur_params}"
-            fi
+            result="$(grep ^collisions ${log_name} | tail -n 1 | sed 's/;.*//')"
+            
+            echo "           Result: ${result}"
+            echo "${result}; Params: ${cur_params}" >> bench_results.temp
         fi
         
         echo ""
@@ -63,22 +58,25 @@ done
 ##### PART TWO #####
 
 # Try a few more options with the best parameters only
-for v in 1 2; do
-    for n in 26 25 24 23 22; do
-        cur_params="${best_params} -n ${n} -v ${v}"
+sort -n -k 2 bench_results.temp | tail -n 3 | sed 's/.*Params: //' | 
+while read best_params; do
+    for n in 25 24 23 22; do
+        cur_params="${best_params} -n ${n}"
         log_name="bench_$(echo ${cur_params} | tr -cd '[:alnum:]').log"
         echo "${cur_params}" > ${log_name}
         
         echo "Currently Testing: ${cur_params}"
         timeout ${bench_length} ${miner_exe} ${base_params} ${cur_params} >> ${log_name} 2>&1
         
-        # Remove the log if the parameters are invalid
-        # Remove the log if the parameters are invalid, else load the speed
+        # Remove the log if the parameters are invalid, else log the speed
         if [[ -n "$(grep ERROR ${log_name})" ]]; then
             echo "           Result: Failure"
             rm ${log_name}
         else
-            echo "           Result: $(grep ^collisions ${log_name} | tail -n 1 | sed 's/;.*//')"
+            result="$(grep ^collisions ${log_name} | tail -n 1 | sed 's/;.*//')"
+            
+            echo "           Result: ${result}"
+            echo "${result}; Params: ${cur_params}" >> bench_results.temp
         fi
         
         echo ""
@@ -89,10 +87,7 @@ done
 ##### PART THREE #####
 
 # Amass the results into a file
-for bench in `find . -name 'bench_*.log'`; do
-    params="$(head -n 1 ${bench})"
-    result="$(grep ^collisions ${bench} | tail -n 1 | sed 's/;.*//')"
-    echo "${result}; Params: ${params}"
-done | sort -n -k 2 > bench_results.log
+sort -n -k 2 bench_results.temp > bench_results.log
 
+rm bench_results.temp
 cat bench_results.log
